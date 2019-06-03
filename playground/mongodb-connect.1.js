6f2d2b48940680f2cb469d308f0efcf8d55674ce
mudi <mudi.muteba@gmail.com>
2019-06-03 19:17:11 +0200

    Initial commit

---
+++ playground/mongodb-connect.1.js
@@ -0,0 +1,45 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+/* var objID = new ObjectID();
+console.log('custom Object ID is', objID); */
+
+// MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, db) => {
+//     if (err)
+//         return console.log('Unable to connect to MongoDB server');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err)
+        return console.log('Unable to connect to MongoDB server');
+    
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    /* db.collection('Todos').insertOne({
+        text: 'A thing',
+        Completed: false
+    }, (err, result) => {
+        if (err)
+            return console.log('Unable to insert todo. Error message:', err);
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    }); */
+
+    //db.collection('Users').insertOne({}, (err, result) => {});
+
+    // db.collection('Users').insertOne({
+    //     name: 'HotSauce',
+    //     age: 2000,
+    //     location: 'Compton'
+    // }, (err, result) => {
+    //     if (err)
+    //         return console.log('Unable to insert user. Error message:', err);
+    //     console.log(JSON.stringify(result.ops, null/*undefined*/, 4));
+    //     console.log('\nTimestamp is', result.ops[0]._id.getTimestamp());
+    // });
+
+
+
+    //db.close(); //old Mongo v2 way
+    client.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err)
+        return console.log('Unable to connect to MongoDB server');
+    
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5cf4faead0a32824a9077d0a')
+    // }).toArray().then( (docs) => {
+    //     console.log('Todos:\n', JSON.stringify(docs, null, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos. Error message:', err);
+    // });
+
+    // db.collection('Todos').find().count().then( (count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos. Error message:', err);
+    // });
+
+    db.collection('Users').find({name: 'HotSauce'}).toArray().then( (docs) => {
+        console.log('HotSauce\'s todos:\n', JSON.stringify(docs, null, 4));
+    }, (err) => {
+        console.log('Unable to fetch todos. Error message:', err);
+    });
+
+    //client.close();
+});
